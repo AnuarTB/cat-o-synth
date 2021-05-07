e0262e0722e3859b9c0ae41b89ec0bd7fd4c00ec
Pollux <a.aidyn00@gmail.com>
2021-05-08 00:45:44 +0900

    creating json file

--- core/bongo-react/src/App.js
+++ core/bongo-react/src/App.js
@@ -1,14 +1,48 @@
-import logo from './logo.svg';
 import './App.css';
+import 'antd/dist/antd.css';
+import axios from 'axios';
 import Table from "./Table";
+import { useState } from 'react';
+import { Button, Radio } from 'antd';
 
 function App() {
-  return (
-    <div>
-        Hello world!
-        <Table rowSize={5} blocks={1} blockSize={16} />
-    </div>
-  );
+    const block_num = 3;
+    let initial_notes = [];
+    for (let i = 0; i < block_num; i ++)
+        initial_notes.push({});
+    console.log(initial_notes);
+    const [notes, setNotes] = useState(initial_notes);
+
+    const addNote = (barId, sectionId, rowId) => {
+        let copy_notes = [...notes];
+        console.log(`bar: ${barId} section: ${sectionId} row: ${rowId}`)
+        console.log(copy_notes);
+        if(copy_notes[barId].hasOwnProperty(sectionId)) {
+            copy_notes[barId][sectionId].push(rowId);
+        } else {
+            copy_notes[barId][sectionId] = [rowId];
+        }
+        console.log(copy_notes);
+        setNotes(copy_notes);
+    }
+
+    const createJson = () => {
+        axios.defaults.headers = {"Api-key": "d2510238-af47-11eb-86a8-0242ac110008"};
+        axios.post("https://json.extendsclass.com/bin", JSON.stringify(notes))
+            .then((res) => {
+                console.log(res.data.uri);
+            });
+    }
+
+    return (
+        <div>
+            Hello world!
+            <Table barNum={block_num} addNote={addNote}/>
+            <Button type="primary" shape="round" size={'large'} onClick={createJson}>
+                Convert
+            </Button>
+        </div>
+    );
 }
 
 export default App;
--- core/bongo-react/src/Table.js
+++ core/bongo-react/src/Table.js
@@ -2,24 +2,27 @@ import logo from './logo.svg';
 import './Table.css';
 
 function RenderCell(props) {
-    let id, barId, section = {props};
+    let {id, barId, section} = props;
     return (
-        <div onClick={() => {console.log(id, barId, section)}} className="cell"></div>
+        <div onClick={() => {
+            console.log(barId, section, id);
+            props.addNote(barId, section, id);
+        }} className="cell"></div>
     );
 }
 function RenderEndCell(props) {
-    let id, barId, section = {props};
+    let {id, barId, section} = props;
     return (
-        <div onClick={() => {console.log(id, barId, section)}} className="cell-end"></div>
+        <div onClick={() => {props.addNote(barId, section, id)}} className="cell-end"></div>
     );
 }
 
 function RenderBar(props) {
     let tmp = [];
     for (let i = 0; i < props.n - 1; i++) {
-        tmp.push(<RenderCell id={props.id} barId={props.barId} section={i} />);
+        tmp.push(<RenderCell id={props.id} barId={props.barId} section={i} addNote={props.addNote} />);
     }
-    tmp.push(<RenderEndCell id={props.id} barId={props.barId} section={props.n-1} />);
+    tmp.push(<RenderEndCell id={props.id} barId={props.barId} section={props.n-1} addNote={props.addNote} />);
     return tmp;
 }
 
@@ -28,7 +31,7 @@ function RenderRow(props) {
     for (let j = 0; j < props.n; j++) {
         let tmp = [];
         for (let i = 0; i < props.k; i++) {
-            tmp.push(<RenderBar n={props.m} id={j} barId={i} barSz={props.k}/>);
+            tmp.push(<RenderBar n={props.m} id={j} barId={i} barSz={props.k} addNote={props.addNote}/>);
         }
         rowElements.push(<div className="row">{tmp}</div>);
     }
@@ -40,7 +43,7 @@ function Table(props) {
     return (
         <div className="table-wrapper">
             
-            <RenderRow n={26} m={16} k={3}/>
+            <RenderRow n={26} m={16} k={props.barNum} addNote={props.addNote}/>
             
         </div>
     );
